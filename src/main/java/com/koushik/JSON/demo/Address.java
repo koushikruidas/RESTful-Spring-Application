025e2dae0bf6fbefdba2548891e738ac777dd609
koushik-ruidas <koushikruidas@gmail.com>
2021-09-21 11:34:28 +0530

    Simple RESTFul application.

---
+++ src/main/java/com/koushik/JSON/demo/Address.java
@@ -0,0 +1,60 @@
+package com.koushik.JSON.demo;
+
+public class Address {
+	private String street;
+	private String city;
+	private String district;
+	private String country;
+	private String state;
+	
+	public Address() {
+		
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	private void setStreet(String street) {
+		this.street = street;
+	}
+
+	private void setCity(String city) {
+		this.city = city;
+	}
+
+	private void setDistrict(String district) {
+		this.district = district;
+	}
+
+	private void setCountry(String country) {
+		this.country = country;
+	}
+
+	private void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", city=" + city + ", district=" + district + ", country=" + country
+				+ ", state=" + state + "]";
+	}
+	
+}
---
+++ src/main/java/com/koushik/JSON/demo/Driver.java
@@ -0,0 +1,35 @@
+package com.koushik.JSON.demo;
+
+
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Student theStudent = mapper.readValue(
+					new File("data/student.json"), Student.class);
+			System.out.println("First Name: "+theStudent.getFirstName());
+			System.out.println("Last Name: "+theStudent.getLastName());
+			System.out.println("ID: "+theStudent.getId());
+			System.out.println("Addres: "+theStudent.getAddress());
+	
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
