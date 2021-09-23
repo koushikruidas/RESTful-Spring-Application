0f0e4676f29f1cb26115ccfc876238b3e4efcf43
koushik-ruidas <koushikruidas@gmail.com>
2021-09-23 17:08:47 +0530

    testing jdbc connection

---
+++ src/main/java/com/koushik/JDBCTest/TestJdbc.java
@@ -0,0 +1,25 @@
+package com.koushik.JDBCTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String jdbcUrl="jdbc:mysql://127.0.0.1:3306/?user=weatherApp-database";
+		String pass="weather";
+		String user="weatherApp-database";
+		System.out.println("Connecting to databse: "+jdbcUrl);
+		try {
+			Connection myCon = DriverManager.getConnection(jdbcUrl, user, pass);
+			System.out.println("Connection successful");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
